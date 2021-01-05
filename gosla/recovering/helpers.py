d942357a862b69796046e25fc6566ab8dab37d3a
Ruslan Ilyasovich Gilfanov <ri.gilfanov@yandex.ru>
2021-01-05 23:38:04 +0500

    0.1a12

--- gosla/recovering/helpers.py
+++ gosla/recovering/helpers.py
@@ -1,26 +1,37 @@
+from base64 import urlsafe_b64decode, urlsafe_b64encode
 from datetime import datetime, timedelta
+from hashlib import sha3_512
+import hmac
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from typing import Any
 
 
-class EmailRecoverHelper:
-    cypher: Any
+class BaseHelper:
+    pass
 
-    def __init__(self, secret):
+
+class TokenHelper(BaseHelper):
+    cypher: 'Any'
+
+    def __init__(self, secret: bytes):
         self.secret = secret
 
-    def gen_token(self, info):
-        expired = str(int((datetime.now() + timedelta(hours=24)).timestamp()))
-        secret: str = self.secret
-        f'{expired}${info}${secret}'.encode('utf-8')
+    def generate_token(self, identity):
+        expired_at = datetime.now() + timedelta(hours=24)
+        expired_at = str(int(expired_at.timestamp()))
+        token_data = f'{expired_at}${identity}'
+        token_data = token_data.encode('utf-8')
+        token_hash = hmac.digest(self.secret, token_data, 'sha3-512')
+        token = f'{token_data}${token_hash}'
+        token = token.encode('utf-8')
+        token = urlsafe_b64encode(token)
+        return token
 
-    def verify_token(self, token):
-        token = self.decode(token)
-
-    def encode(self, data: str):
-        pass
-
-    def decode(self, token: bytes):
-        pass
+    def verify_token(self, token: str):
+        token = token.encode('utf-8')
+        token = urlsafe_b64decode(token)
+        token = token.decode('utf-8')
+        identity, expired = token.split('$')
+        return identity, expired
